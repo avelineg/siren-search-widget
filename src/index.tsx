0418542c42896a617ae59e3ceba2bf798f564deb
avelineg <a.guenneugues@cmexpert.fr>
2025-06-27 18:22:06 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-const container = document.getElementById("root");
-if (container) {
-  const root = createRoot(container);
-  root.render(<App />);
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
