2b4cd9f35dff9a66f2f858671122c20565a6bc4d
avelineg <a.guenneugues@cmexpert.fr>
2025-07-01 16:04:19 +0200

    Update decode.ts

--- src/logic/decode.ts
+++ src/logic/decode.ts
@@ -1,10 +1,7 @@
-import formeJuridique from "../formeJuridique.json";
-import naf from "../naf.json";
-
-export function decodeFormeJuridique(code: string) {
-  return formeJuridique[code] || code;
+export function decodeNatureJuridique(code: string, data: Record<string, string>) {
+  return data[code] || code;
 }
 
-export function decodeNaf(code: string) {
-  return naf[code] || code;
+export function decodeNAF(code: string, data: Record<string, string>) {
+  return data[code] || code;
 }
