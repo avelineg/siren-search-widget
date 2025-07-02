def70d2bcf51585109bd4789dff5c856f328cf18
avelineg <a.guenneugues@cmexpert.fr>
2025-07-02 11:30:35 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Active le plugin React pour gérer TSX/JSX et le rechargement à chaud
   plugins: [react()],
-
-  build: {
-    outDir: 'dist',
+  server: {
+    port: 5173
   },
-
-  // Copie tout le contenu de public/ (dont favicon.ico) vers dist/
-  publicDir: 'public',
+  define: {
+    'process.env': process.env
+  }
 })
