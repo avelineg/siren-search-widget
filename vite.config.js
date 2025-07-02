f086c2a9b4e069dc894ef3ad349ec7791b82b111
avelineg <a.guenneugues@cmexpert.fr>
2025-07-02 08:42:31 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-   build: {
-     outDir: 'dist',
-   },
-+  publicDir: 'public' // assure la copie de tout ce qui est dans public/, y compris favicon.ico
- })
+  build: {
+    outDir: "dist",
+  },
+  // Assure la copie de tout ce qui est dans public/, y compris favicon.ico
+  publicDir: "public",
+})
