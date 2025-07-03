3a5ce0b7434ae8b37628d5085c41e46b501ecf8b
avelineg <a.guenneugues@cmexpert.fr>
2025-07-03 14:28:55 +0200

    Update useEtablissementData.ts

--- src/hooks/useEtablissementData.ts
+++ src/hooks/useEtablissementData.ts
@@ -13,21 +13,29 @@ export function useEtablissementData(query: string, selectedCode: string = '') {
     setResults([])
     if (!query || query.length < 3) return
     setLoading(true)
+    console.log("[useEtablissementData] Recherche lancée, query:", query)
     const isSiret = /^\d{14}$/.test(query)
     const isSiren = /^\d{9}$/.test(query)
     if (isSiret || isSiren) {
       fetchEtablissementByCode(query)
-        .then(res => setData(res))
-        .catch(err => setError(err.message))
+        .then(res => {
+          console.log("[useEtablissementData] Résultat fetchEtablissementByCode:", res)
+          setData(res)
+        })
+        .catch(err => {
+          console.error("[useEtablissementData] Erreur fetchEtablissementByCode:", err)
+          setError(err.message)
+        })
         .finally(() => setLoading(false))
     } else {
-      // Recherche par nom
       searchEtablissementsByName(query)
         .then(list => {
+          console.log("[useEtablissementData] Résultat searchEtablissementsByName:", list)
           setResults(list)
           setLoading(false)
         })
         .catch(err => {
+          console.error("[useEtablissementData] Erreur searchEtablissementsByName:", err)
           setError(err.message)
           setLoading(false)
         })
@@ -39,9 +47,16 @@ export function useEtablissementData(query: string, selectedCode: string = '') {
       setLoading(true)
       setError(null)
       setData(null)
+      console.log("[useEtablissementData] Recherche détaillée, selectedCode:", selectedCode)
       fetchEtablissementByCode(selectedCode)
-        .then(res => setData(res))
-        .catch(err => setError(err.message))
+        .then(res => {
+          console.log("[useEtablissementData] Résultat fetchEtablissementByCode (selectedCode):", res)
+          setData(res)
+        })
+        .catch(err => {
+          console.error("[useEtablissementData] Erreur fetchEtablissementByCode (selectedCode):", err)
+          setError(err.message)
+        })
         .finally(() => setLoading(false))
     }
   }, [selectedCode])
