9f6a028dca40da28c90515fb72ae66ff3b3b4b3e
avelineg <a.guenneugues@cmexpert.fr>
2025-07-28 09:17:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -321,7 +321,7 @@ function App() {
                   title="Télécharger l'avis de situation INSEE (ouvre un PDF officiel)"
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width={18} height={18}>
-                    <path fillRule="evenodd" d="M9.25 2.75a.75.75 0 0 1 1.5 0v7.69l2.22-2.22a.75.75 0 1 1 1.06 1.06l-3.5 3.5a.75.75 0 0 1-1.06 0l-3.5-3.5a.75.75 0 1 1 1.06-1.06l2.22 2.22V2.75ZM3.75 15[...]
+                    <path fillRule="evenodd" d="M9.25 2.75a.75.75 0 0 1 1.5 0v7.69l2.22-2.22a.75.75 0 1 1 1.06 1.06l-3.5 3.5a.75.75 0 0 1-1.06 0l-3.5-3.5a.75.75 0 1 1 1.06-1.06l2.22 2.22V2.75ZM3.75 15a.75.75 0 0 1 0-1.5h12.5a.75.75 0 0 1 0 1.5H3.75Z" clipRule="evenodd" />
                   </svg>
                   Télécharger l'avis de situation INSEE (PDF)
                 </button>
