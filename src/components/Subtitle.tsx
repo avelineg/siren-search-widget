d880926784f12d6657016fdd2942191c6b6f45f0
avelineg <a.guenneugues@cmexpert.fr>
2025-09-01 11:13:14 +0200

    Delete src/components/Subtitle.tsx

--- src/components/Subtitle.tsx
+++
@@ -1,6 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-
-export function Subtitle({ children }: PropsWithChildren) {
-return <p className="m-0 mb-2 text-[12.5px] text-gray-500">{children}</p>;
-}
